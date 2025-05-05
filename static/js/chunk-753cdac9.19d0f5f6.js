41ad18aac9dc4e9c566f40346b199aedc69acff4
put <put@budingcat.xyz>
2025-05-05 16:24:07 +0800

    externalLink

--- static/js/chunk-753cdac9.19d0f5f6.js
+++ static/js/chunk-753cdac9.19d0f5f6.js
@@ -152,7 +152,10 @@
             }, {
                 menuTitle: o["a"].t("使用文档"),
                 menuIcon: "book-open-text",
-                menuPath: "/stage/knowledge"
+                // menuPath: "/stage/knowledge"
+                menuPath: btoa("https://docs.budingcat.xyz/docs/category/%E5%BC%80%E5%A7%8B%E4%BD%BF%E7%94%A8/"),
+                externalLink: "https://docs.budingcat.xyz/docs/category/%E5%BC%80%E5%A7%8B%E4%BD%BF%E7%94%A8/",
+                isExtraMenu: !0
             }]
         }, {
             groupTitle: o["a"].t("订阅"),
